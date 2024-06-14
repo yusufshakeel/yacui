963a1136c9c9b48f1728653ff7e105fc77865973
Yusuf Shakeel <6522504+yusufshakeel@users.noreply.github.com>
2024-06-15 00:06:17 +0530

    refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-export * from './components/Footer';
 export * from './components/Modal';
-export * from './components/Row';
-export * from './components/TitleBar';
 export * from './components/Window';
 export * as CHARACTERS from './constants/characters';
